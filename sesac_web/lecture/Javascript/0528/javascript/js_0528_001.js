cd63492d961be461fd1ac005249d77952b731321
Yang tae wan <137491948+behindy3359@users.noreply.github.com>
2024-05-28 20:32:43 +0900

    selfstudy:0528-2 hoisting test

---
+++ sesac_web/lecture/Javascript/0528/javascript/js_0528_001.js
@@ -0,0 +1,8 @@
+console.log("vNoodle 에 할당된 값 :" + vNoodle);
+console.log("cNoodled 에 할당된 값 :" + cNoodle);
+var vNoodle = "ramen" ;
+let lNoodle = "udong";
+const cNoodle = "soba";
+console.log("vNoodle 에 할당된 값 :" + vNoodle);
+console.log("lNoodled 에 할당된 값 :" + lNoodle);
+console.log("cNoodled 에 할당된 값 :" + cNoodle);
