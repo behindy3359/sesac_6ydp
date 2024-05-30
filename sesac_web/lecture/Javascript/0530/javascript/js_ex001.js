bd3fd22fae28ca624c8f422cc14edd9d394867ed
Yang tae wan <137491948+behindy3359@users.noreply.github.com>
2024-05-31 00:13:45 +0900

    selfstudy : 0531-1 if,function

---
+++ sesac_web/lecture/Javascript/0530/javascript/js_ex001.js
@@ -0,0 +1,13 @@
+function multifly( a , b ) {
+    return a * b ;
+}
+console.log(multifly(3,7));
+console.log(multifly(2,2));
+
+function square( a ) {
+    console.log(a**2) ;
+}
+square(4)
+square(11)
+square(5)
+
