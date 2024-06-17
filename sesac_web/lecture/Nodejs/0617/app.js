56a2f26b2ffb978a21901d47d2a7eda7fb6dde30
behindy3359 <solme47@gmail.com>
2024-06-17 17:20:52 +0900

    study : 0617 - 4

--- sesac_web/lecture/Nodejs/0617/app.js
+++ sesac_web/lecture/Nodejs/0617/app.js
@@ -1,5 +1,6 @@
 const expressM = require('express');
 const app = expressM();
+
 portNo = 8080;
 
 app.set('view engine', 'ejs');
@@ -20,7 +21,24 @@ app.post('/ajax', (req, res)=>{
   console.log(req.body);
   res.send(req.body);
 })
-
+app.get('/axios', (req, res)=>{
+  console.log(req.query);
+  res.send(req.query);
+})
+app.post('/axios', (req, res)=>{
+  console.log(req.body);
+  res.send(req.body);
+})
+app.get('/fetch', (req, res)=>{
+  console.log(req.query);
+  res.send(req.query);
+})
+app.post('/fetch', (req, res)=>{
+  console.log(req.body);
+  res.send(req.body);
+})
 app.listen(portNo,()=>{
   console.log(`${portNo}에서 서버를 열겠습니다. ${Date()}`);
 })
+
+
