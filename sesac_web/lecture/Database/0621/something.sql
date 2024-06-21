e8cb9975715f46b72378c6a81a0ac8a4a7a25954
behindy3359 <solme47@gmail.com>
2024-06-21 18:44:53 +0900

    study : 0621 - 5
    database DML DDL DCL

--- sesac_web/lecture/Database/0621/something.sql
+++ sesac_web/lecture/Database/0621/something.sql
@@ -259,18 +259,50 @@ select custid, sum(amount), sum(price * amount)
 	from orders 
 		where custid != 'bunny'
 		group by custid having sum(price * amount) >= 10000;
-
 /*
 where vs. having
-
 having
 - 그룹에 대해서 필터링 (그래서 group by 와 함께 쓰임)
 - group by 보다 뒤에 위치해야 함
 - 집계 함수랑 함께 사용 가능
-
 where 
 - 각각의 행을 필터링
 - group by 보다 앞에 위치
 - 집계함수를 쓸 수는 있으나 having 절 보다는 자유롭지 못함
-
 */
+
+select * from orders;
+select * from customer; 
+
+-- join 여러 테이블을 합쳐서 정보를 조회할 때 사용!
+
+select count(*) from customer; -- 15
+select count(*) from orders; -- 21
+select count(*) from customer, orders; -- 315 = 15 * 21
+
+-- where 절을 이용해 조인 조건을 추가
+select * from customer, orders 
+	where customer.custid = orders.custid;
+    
+select * from customer inner join orders 
+	on customer.custid = orders.custid;
+
+-- "고객 이름"과 고객이 주문한 "상품명", "상품 가격" 조회
+select custname, prodname, price from customer, orders 
+	where customer.custid = orders.custid;
+    
+select custname, prodname, price from customer inner join orders 
+	on customer.custid = orders.custid;
+
+-- 고객 이름별로 주문한 제품 총 구매액을 고객별로 // 오름차순 정렬
+
+select custname, sum( price * amount )as 'total_price' 
+	from customer inner join orders 
+	on customer.custid = orders.custid 
+    group by custname 
+    order by total_price desc;
+    
+select * from customer natural join orders; 
+
+
+
